ca5ac4c43ec49edf82b5533b64957abd407f27c6
softwareengineerjoe <maranan.mcjo@gmail.com>
2023-06-04 11:37:22 +0800

    add scroll behavior

--- src/pages/major/index.jsx
+++ src/pages/major/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import constants from "../../constants.js";
+import { UpSquareOutlined } from "@ant-design/icons";
 
 const { majorArcana } = constants;
 console.log(majorArcana);
@@ -7,8 +8,10 @@ console.log(majorArcana);
 export default function index() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center text-justify gap-6 py-8 px-12 font-playfair text-gray-200 bg-[#2a3042]">
-        <h1 className="text-3xl">Major Arcana Cards</h1>
+      <div className={mainBox}>
+        <h1 className="text-3xl" id="top">
+          Major Arcana Cards
+        </h1>
         {majorArcana.map((card) => (
           <div
             key={card.code}
@@ -16,21 +19,26 @@ export default function index() {
           >
             <h3 className={title}>{card.card}</h3>
             <img src={card.img} alt={card.card} className="w-32" />
-            <h3 className={subtitle}>
-              {card.card} Upright Position
-            </h3>
+            <h3 className={subtitle}>{card.card} Upright Position</h3>
             <p className={description}>{card.description.upright}</p>
-            <h3 className={subtitle}>
-              {card.card} Reversed Position
-            </h3>
+            <h3 className={subtitle}>{card.card} Reversed Position</h3>
             <p className={description}>{card.description.reversed}</p>
           </div>
         ))}
+        <a
+          href="#top"
+          className="fixed bottom-3 right-2 lg:bottom-8 lg:right-10"
+        >
+          <UpSquareOutlined style={{ fontSize: "30px", color: "#c3a38c" }} className="lg:hidden"/>
+          <UpSquareOutlined style={{ fontSize: "40px", color: "#c3a38c" }} className="hidden lg:block"/>
+        </a>
       </div>
     </>
   );
 }
 
-const title = "text-2xl text-[#c3a38c]";
-const subtitle = "text-lg text-[#c3a38c]"
-const description = "text-sm"
+const title = "text-2xl text-[#c3a38c] text-center";
+const subtitle = "text-lg text-[#c3a38c] text-center";
+const description = "indent-8 text-sm text-justify";
+const mainBox =
+  "flex flex-col justify-center items-center text-justify gap-6 py-8 px-12 lg:px-32 font-playfair text-gray-200 bg-[#2a3042] relative";
